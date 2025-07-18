d396c829d53bf8628637a74cde8712175e8b81af
ParthVaghasiyakal-072 <parthvaghasiya.kalthiyainfo@gmail.com>
2025-07-18 14:21:22 +0530

    asdasd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,7 +57,7 @@ function App() {
 
           {/* <Route path='/top' element={<TopHeader />} /> */}
           <Route path='/sidebar' element={<Sidebar2 />} >
-            
+              
           </Route >
 
 
