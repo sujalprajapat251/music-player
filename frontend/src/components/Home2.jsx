ad7a758793e5a8624e14782617cfa47e93f113d6
ParthVaghasiyakal-072 <parthvaghasiya.kalthiyainfo@gmail.com>
2025-07-21 09:25:28 +0530

    Parth-18-07-2025

--- frontend/src/components/Home2.jsx
+++ frontend/src/components/Home2.jsx
@@ -147,8 +147,8 @@ const Home2 = () => {
 
     return (
         <>
-            <div className="p-10 bg-[#141414]">
-                <div className="flex gap-3 text-white top-0 left-0 bg-[#141414] pt-3 py-4">
+            <div className=" lg:p-5 xl:p-6 2xl:p-8 3xl:p-10 bg-[#141414]">
+                <div className="flex gap-3 text-white top-0 left-0 bg-[#141414] xl:pt-2 xl:py-2 2xl:pt-3 2xl:py-4">
                     {/* Mobile Menu Button - Only visible on mobile */}
                     <div className="md:hidden mb-4">
                         <button
@@ -161,50 +161,49 @@ const Home2 = () => {
 
                     {/* Main Content */}
                     <div className="">
-                        <h1 className="text-2xl font-bold ">My Projects</h1>
+                        <h1 className="lg:text-[20px] 2xl:text-[24px] 3xl:text-[30px] font-bold ">My Projects</h1>
                     </div>
                 </div>
 
                 <div className='max-h-[80vh] overflow-auto d_customscrollbar'>
-                    <div className="flex mt-5 gap-10 ">
+                    <div className="flex lg:mt-3 xl:mt-4 3xl:mt-5 xl:gap-8 2xl:gap-10 ">
                         <div >
-                            <p className="text-white text-[24px] font-[600]">Start a new project</p>
-                            <p className="text-white text-[16px]">Create a music or podcast project.</p>
-                            <div className='flex bg-black mt-4 h-[200px] w-[250px] d_customborder items-center justify-center'>
-                                <button className='border border-dashed border-white flex flex-col items-center justify-center group p-2 rounded-xl hover:bg-gray-900' >
-                                    <p className="text-white text-[24px]">+</p>
-                                    <p className="text-white text-[14px]">
+                            <p className="text-white  lg:text-[18px] 2xl:text-[20px] 3xl:text-[24px] font-[600]">Start a new project</p>
+                            <p className="text-white lg:text-[14px] 3xl:text-[15px] xl:text-nowrap">Create a music or podcast project.</p>
+                            <div className='flex bg-black lg:mt-3 3xl:mt-4  xl:h-[180px] xl:w-[220px] 2xl:h-[180px] 2xl:w-[200px] 3xl:h-[200px] 3xl:w-[250px] d_customborder items-center justify-center'>
+                                <button className='border border-dashed border-white flex flex-col items-center justify-center group  lg:p-2 rounded-xl hover:bg-gray-900' >
+                                    <p className="text-white xl:text-[24px]">+</p>
+                                    <p className="text-white xl:text-[14px]">
                                         New Project
                                     </p>
                                 </button>
                             </div>
-
                         </div>
                         <div>
                             <div className='flex justify-between'>
                                 <div>
-                                    <p className="text-white text-[24px] font-[600]">Explore demo projects</p>
-                                    <p className="text-white text-[16px]">Play around with professionally-made songs.</p>
+                                    <p className="text-white xl:text-[18px] 2xl:text-[20px] 3xl:text-[24px] font-[600]">Explore demo projects</p>
+                                    <p className="text-white 2xl:text-[14px] 3xl:text-[15px]">Play around with professionally-made songs.</p>
                                 </div>
                                 <div className='my-auto'>
                                     <Link to='/layout/demo-project'>
-                                        <button className='py-2 px-6 border rounded-3xl border-[#FFFFFF1A] hover:bg-gray-600'>
+                                        <button className='xl:py-2 xl:px-6 border rounded-3xl border-[#FFFFFF1A] hover:bg-gray-600'>
                                             <p className='text-white'>Show all</p>
                                         </button>
                                     </Link>
                                 </div>
                             </div>
                             <div>
-                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-6 mt-5">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 xl:gap-4 3xl:gap-6 xl:mt-3 3xl:mt-5">
                                     {sounds.map((sound, index) => (
                                         <div key={sound._id || index} className="bg-[#14141480] rounded-[4px] overflow-hidden d_customborder">
-                                            <div className='w-full h-[135px]'>
+                                            <div className='w-full xl:h-[125px] 2xl:h-[124px] 3xl:h-[135px]'>
                                                 <img src={`${IMAGE_URL}uploads/image/${sound?.image}`} alt="Album" className="w-full h-full object-cover" />
                                             </div>
-                                            <div className="py-[8px] px-[12px]">
+                                            <div className="xl:py-[6px] xl:px-[8px] 2xl:px-[10px] 3xl:py-[8px] 3xl:px-[12px]">
                                                 <div className="flex justify-between items-center">
                                                     <div>
-                                                        <h3 className="text-[#fff] font-[500] text-[16px] mb-[2px]">{sound?.soundname}</h3>
+                                                        <h3 className="text-[#fff] font-[500] 2xl:text-[14px] 3xl:text-[16px] mb-[2px]">{sound?.soundname}</h3>
                                                         <p className="text-[#FFFFFF99] font-[400] text-[14px]">{sound?.soundtype}</p>
                                                     </div>
                                                     <button
@@ -229,27 +228,27 @@ const Home2 = () => {
                     </div>
 
 
-                    <div className='mt-14 flex justify-between pb-5 border-b border-[#FFFFFF1A]'>
+                    <div className='xl:mt-10 2xl:mt-12 3xl:mt-14 flex justify-between xl:pb-3  2xl:pb-4 3xl:pb-5 border-b border-[#FFFFFF1A]'>
                         <div className='my-auto'>
-                            <button className='py-2 px-6 border rounded-3xl border-[#FFFFFF1A] hover:bg-gray-600 cursor-pointer' onClick={() => setAddFolderModal(true)}>
+                            <button className='xl:py-2 xl:px-6 border rounded-3xl border-[#FFFFFF1A] hover:bg-gray-600 cursor-pointer' onClick={() => setAddFolderModal(true)}>
                                 <p className='text-white'>+ Add Folder</p>
                             </button>
                         </div>
                         <div className="flex">
                             {activeSearch === true ?
                                 <div className='bg-[#FFFFFF0F]'>
-                                    <div className="flex gap-5 py-2 px-5 justify-center">
-                                        <FiSearch className="text-white text-[24px]" />
+                                    <div className="flex xl:gap-4 2xl:gap-5 xl:py-2 xl:px-5 justify-center">
+                                        <FiSearch className="text-white xl:text-[24px]" />
                                         <input type="text" className='outline-none border-0 bg-transparent text-white' placeholder="Search..." />
                                         <IoClose size={24} className="text-white ms-auto cursor-pointer" onClick={() => setActiveSearch(false)} />
                                     </div>
                                 </div>
                                 :
                                 <>
-                                    <div className='flex relative pe-2' ref={sortDropdownRef}>
+                                    <div className='flex relative xl:pe-2' ref={sortDropdownRef}>
                                         <button
                                             onClick={toggleSortDropdown}
-                                            className='flex items-center gap-3 text-white cursor-pointer hover:text-gray-300 transition-colors'
+                                            className='flex items-center xl:gap-3 text-white cursor-pointer hover:text-gray-300 transition-colors'
                                         >
                                             <span>Sort by : {sortBy}</span>
                                             <IoIosArrowDown
@@ -260,20 +259,20 @@ const Home2 = () => {
 
                                         {/* Dropdown Menu */}
                                         {isSortDropdownOpen && (
-                                            <div className="absolute top-full right-0 mt-2 bg-[#1F1F1F] rounded-lg shadow-lg z-10 min-w-[200px]">
+                                            <div className="absolute top-full right-0 xl:mt-2 bg-[#1F1F1F] rounded-lg shadow-lg z-10 min-w-[200px]">
                                                 {sortOptions.map((option) => (
                                                     <div
                                                         key={option.value}
                                                         onClick={() => handleSortSelect(option)}
-                                                        className="flex items-center px-4 py-3 hover:bg-[#3b3b3b] cursor-pointer transition-colors"
+                                                        className="flex items-center xl:px-3 xl:py-2 2xl:px-4 2xl:py-3 hover:bg-[#3b3b3b] cursor-pointer transition-colors"
                                                     >
                                                         <div className="flex items-center">
-                                                            <div className={`w-4 h-4 border-2 rounded-full mr-3 flex items-center justify-center ${sortBy === option.value
+                                                            <div className={`xl:w-3 xl:h-3 2xl:w-4 2xl:h-4 border-2 rounded-full xl:mr-2 2xl:mr-3 flex items-center justify-center ${sortBy === option.value
                                                                 ? 'border-white'
                                                                 : 'border-[#FFFFFF40]'
                                                                 }`}>
                                                                 {sortBy === option.value && (
-                                                                    <div className="w-2 h-2 bg-white rounded-full"></div>
+                                                                    <div className="xl:w-1 xl:h-1 2xl:w-2 2xl:h-2 bg-white rounded-full"></div>
                                                                 )}
                                                             </div>
                                                             <span className="text-white text-sm">{option.label}</span>
@@ -283,28 +282,28 @@ const Home2 = () => {
                                             </div>
                                         )}
                                     </div>
-                                    <div className='my-auto px-3 cursor-pointer' onClick={() => setActiveSearch(true)}>
-                                        <FiSearch className="text-white text-[24px]" />
+                                    <div className='my-auto xl:px-2 2xl:px-3 cursor-pointer' onClick={() => setActiveSearch(true)}>
+                                        <FiSearch className="text-white xl:text-[20px] 2xl:text-[24px]" />
                                     </div>
                                 </>
                             }
-                            <div className='my-auto px-3' >
+                            <div className='my-auto xl:px-2 2xl:px-3' >
                                 <Menu as="div" className="relative inline-block text-left ">
                                     <div>
                                         <MenuButton className="outline-none" >
-                                            <BsThreeDotsVertical className='text-white ' />
+                                            <BsThreeDotsVertical className='text-white  xl:text-[18px] 3xl:text-[20px]' />
                                         </MenuButton>
                                     </div>
                                     <MenuItems
                                         transition
-                                        className="absolute right-0 md:mt-3 2xl:mt-3  z-30 w-40 2xl:w-60 origin-top-right  bg-[#1f1f1f] shadow-lg outline-none rounded-md"
+                                        className="absolute right-0 md:mt-3 2xl:mt-3  z-30 xl:w-52 2xl:w-60 origin-top-right  bg-[#1f1f1f] shadow-lg outline-none rounded-md"
                                     >
                                         <div className="">
                                             <MenuItem >
                                                 <p className="block md:px-5 lg:px-6 md:py-1  2xl:px-7 xl:py-2  3xl:px-9 3xl:py-3   hover:bg-gray-800 cursor-pointer" >
                                                     <div className="flex">
-                                                        <img src={DeleteIcon} alt="" />
-                                                        <p className="text-white md:ps-2 lg:ps-3 xl:ps-4 3xl:ps-4 font-semibold text-[14px] xl:text-[16px]">Recently Deleted</p>
+                                                        <img src={DeleteIcon} alt="" className='xl:w-4 xl:h-4 2xl:w-6 2xl:h-6' />
+                                                        <p className="text-white md:ps-2 lg:ps-3 xl:ps-4 3xl:ps-4 font-semibold xl:text-[14px] 2xl:text-[16px] ">Recently Deleted</p>
                                                     </div>
                                                 </p>
                                             </MenuItem>
@@ -317,14 +316,14 @@ const Home2 = () => {
                     </div>
 
                     {folders?.map((ele, index) => (
-                        <div key={ele._id} className="flex pt-5 ps-5 pe-2">
-                            <img src={folder} alt="" className='w-[30px] h-[30px]' />
-                            <p className="text-white ps-5 my-auto">{ele?.folderName}</p>
+                        <div key={ele._id} className="flex xl:pt-3 xl:ps-3 2xl:pt-4 2xl:ps-4 3xl:pt-5 3xl:ps-5 xl:pe-2">
+                            <img src={folder} alt="" className='xl:w-[30px] xl:h-[30px]' />
+                            <p className="text-white xl:ps-4 xl:ps-5 my-auto">{ele?.folderName}</p>
                             <div className='ms-auto'>
                                 <Menu as="div" className="relative inline-block text-left ">
                                     <div>
                                         <MenuButton className="outline-none" >
-                                            <BsThreeDotsVertical className='text-white text-[20px]' />
+                                            <BsThreeDotsVertical className='text-white xl:text-[18px] 3xl:text-[20px]' />
                                         </MenuButton>
                                     </div>
                                     <MenuItems
@@ -337,8 +336,8 @@ const Home2 = () => {
                                                     onClick={() => handleRenameClick(ele._id, ele.folderName)}
                                                 >
                                                     <div className="flex">
-                                                        <img src={rename} alt="" />
-                                                        <p className="text-white md:ps-2 lg:ps-3 xl:ps-4 3xl:ps-4 font-semibold text-[14px] xl:text-[16px]">Rename</p>
+                                                        <img src={rename} alt="" className='xl:w-4 xl:h-4 2xl:w-6 2xl:h-6' />
+                                                        <p className="text-white md:ps-2 lg:ps-3 xl:ps-4 3xl:ps-4 font-semibold xl:text-[14px] 2xl:text-[16px]">Rename</p>
                                                     </div>
                                                 </p>
                                             </MenuItem>
@@ -347,8 +346,8 @@ const Home2 = () => {
                                                     onClick={() => handleDeleteClick(ele._id)}
                                                 >
                                                     <div className="flex">
-                                                        <img src={RedDelete} alt="" />
-                                                        <p className="text-[#FF0000] md:ps-2 lg:ps-3 xl:ps-4 3xl:ps-4 font-semibold text-[14px] xl:text-[16px]">Delete</p>
+                                                        <img src={RedDelete} alt="" className='xl:w-4 xl:h-4 2xl:w-6 2xl:h-6' />
+                                                        <p className="text-[#FF0000] md:ps-2 lg:ps-3 xl:ps-4 3xl:ps-4 font-semibold xl:text-[14px] 2xl:text-[16px]">Delete</p>
                                                     </div>
                                                 </p>
                                             </MenuItem>
