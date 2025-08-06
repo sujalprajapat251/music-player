bf231ae2802d39e91d88eb5560178db4850ca2f6
ParthVaghasiyakal-072 <parthvaghasiya.kalthiyainfo@gmail.com>
2025-08-06 14:02:44 +0530

    Parth06-08

--- frontend/src/components/AutoWah.jsx
+++ frontend/src/components/AutoWah.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { FaPowerOff } from 'react-icons/fa6';
 import { IoClose } from 'react-icons/io5';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeEffect} from '../Redux/Slice/effects.slice';
+import { removeEffect } from '../Redux/Slice/effects.slice';
 
 function polarToCartesian(cx, cy, r, angle) {
     const a = (angle - 90) * Math.PI / 180.0;
@@ -197,9 +197,15 @@ function Knob1({ label = "Bite", min = -135, max = 135, defaultAngle }) {
 
 const AutoWah = () => {
 
+    const [isPoweredOn, setIsPoweredOn] = useState(true);
+
+    const handlePowerToggle = () => {
+        setIsPoweredOn(!isPoweredOn);
+    };
+
     const dispatch = useDispatch();
-    const { activeEffects} = useSelector((state) => state.effects);
-    
+    const { activeEffects } = useSelector((state) => state.effects);
+
     const handleRemoveEffect = (instanceId) => {
         dispatch(removeEffect(instanceId));
     };
@@ -210,12 +216,23 @@ const AutoWah = () => {
 
     return (
         <div className='bg-[#141414]'>
-            <div className='flex justify-between items-center w-[150px] h-[40px] sm:w-[190px] sm:h-[50px] md600:w-[220px] md:w-[230px] md:h-[55px] lg:w-[240px] xl:h-[60px]  2xl:w-[256px] 2xl:h-[64px] rounded-t-lg bg-[#409C9F] px-3'>
-                <FaPowerOff className='text-white text-[16px] md600:text-[20px]' />
+            <div className={`flex justify-between items-center w-[150px] h-[40px] sm:w-[190px] sm:h-[50px] md600:w-[220px] md:w-[230px] md:h-[55px] lg:w-[240px] xl:h-[60px] 2xl:w-[256px] 2xl:h-[64px] rounded-t-lg px-2 md:px-3 transition-colors duration-300 ${isPoweredOn ? 'bg-[#409C9F]' : 'bg-gray-600'
+                }`}>
+                <FaPowerOff
+                    className={`text-[16px] md600:text-[20px] cursor-pointer transition-colors duration-200 ${isPoweredOn ? 'text-white hover:text-purple-400' : 'text-red-500 hover:text-red-400'
+                        }`}
+                    onClick={handlePowerToggle}
+                />
                 <p className='text-white text-[12px] md600:text-[16px]'>Auto-Wah</p>
-                <IoClose className='text-white text-[16px] md600:text-[20px] hover:text-[#ff0000]' onClick={() => handleRemoveEffect(currentInstanceId)} />
+                <IoClose
+                    className={`text-[16px] md600:text-[20px] ${isPoweredOn
+                        ? 'text-white hover:text-purple-400'
+                        : 'text-white hover:text-red-500'
+                        }`}
+                    onClick={() => handleRemoveEffect(currentInstanceId)}
+                />
             </div>
-            <div className='w-[150px] h-[140px] sm:w-[190px] sm:h-[180px] md600:w-[220px] md600:h-[210px] md:w-[230px] md:h-[265px] lg:w-[240px] lg:h-[282px] xl:w-[240px] xl:h-[285px] 2xl:w-[256px] 2xl:h-[300px] bg-[#302f2f] relative'>
+            <div className={`w-[150px] h-[140px] sm:w-[190px] sm:h-[180px] md600:w-[220px] md600:h-[210px] md:w-[230px] md:h-[265px] lg:w-[240px] lg:h-[282px] xl:w-[240px] xl:h-[285px] 2xl:w-[256px] 2xl:h-[300px] bg-[#302f2f] relative ${!isPoweredOn ? 'opacity-50 pointer-events-none' : ''}`}>
                 {/* Tone Knob - Top Right */}
                 <div className="absolute top-[50px] right-[30px] sm:top-[50px] sm:right-[35px] md600:top-[50px] md600:right-[50px] md:top-[60px] md:right-[50px]  xl:top-[50px] xl:right-[50px]">
                     <Knob1 label="Rate" min={-135} max={135} defaultAngle={0} />
